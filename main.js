a7a31b64d18c71c70836a347281e80ce8451eb77
user <user@users-MacBook-Pro.local>
2023-06-12 13:08:43 -0700

    testing connection to git

---
+++ main.js
@@ -0,0 +1,11 @@
+//Write a function that calculates and returns the square of a number, which is passed in as a parameter.  This function will take one parameter, and return that parameter multiplied by itself.  
+
+function calculateSquareNum(num1) {
+  return num1 * num1;
+}
+
+// Write a function that raises a number x to the power y, and returns the result.  This function will take two parameters, x and y, and return the result.
+
+function raiseNumPower(x, y) {
+  return Math.pow(x, y);
+}
